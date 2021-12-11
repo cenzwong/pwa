4a31ea50f8637f65aa55452ab6c05a326f4f74c7
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-12-11 18:12:32 +0800

    update

--- sw.js
+++ sw.js
@@ -1,58 +1,53 @@
-var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/pwa/',                     // If you have separate JS/CSS files,
-  '/pwa/index.html'            // add path to those files here
-]
+let cacheName = "OpenGithubPWA";// 👈 any unique name
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+let filesToCache = [
+  "/OpenPWA/", // 👈 your repository name , both slash are important
+  "service-worker.js",
+  "js/main.js",
+  "js/install-handler.js",
+  "js/settings.js",
+  "css/main.css",
+  "assets/icons/icon.png",
+  "manifest.json"
+  // add your assets here 
+  // ❗️❕donot add config.json here ❗️❕
+];
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
-})
+self.addEventListener("install", function (event) {
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    console.log('installed successfully')
+    return cache.addAll(filesToCache);
+  }));
+});
 
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
+self.addEventListener('fetch', function (event) {
 
-// Delete outdated caches
+  if (event.request.url.includes('clean-cache')) {
+    caches.delete(cacheName);
+    console.log('Cache cleared')
+  }
+
+  event.respondWith(caches.match(event.request).then(function (response) {
+    if (response) {
+      console.log('served form cache')
+    } else {
+      console.log('Not serving from cache ', event.request.url)
+    }
+    return response || fetch(event.request);
+  })
+  );
+});
+
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          console.log('service worker: Removing old cache', key);
+          return caches.delete(key);
         }
-      }))
+      }));
     })
-  )
-})
+  );
+  return self.clients.claim();
+});
